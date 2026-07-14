bca1a416a9322a24c396cbedb8dff6a545b19e77
danielgolonko07-del <danielgolonko07@gmail.com>
2026-07-14 15:41:43 +0200

    added a few ne functions: add and delate tasks and new list with done tasks

--- to-do-list.js
+++ to-do-list.js
@@ -1,15 +1,66 @@
 const inputTask = document.getElementById('inputTask');
 const taskListElement = document.getElementById('taskList')
+const doneTaskElement = document.getElementById('doneTask')
 let taskList = []
+let completedTasks = []
+let doneTask = []
 
 function renderTaskList() {
   taskListElement.innerHTML = '';
-  taskList.forEach(task => {
-    const li = document.createElement('li')
 
-    li.textContent = task;
-    taskListElement.appendChild(li);
+  taskList.forEach(task => {
+    const li = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const text = document.createElement('span');
+    text.innerText = task;
+
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        text.style.textDecoration = 'line-through';
+      } else {
+        text.style.textDecoration = 'none';
+      }
+    });
+
+    const doneButton = document.createElement('button');
+    doneButton.innerText = 'Done';
+
+    checkbox.addEventListener('click', () => {
+      const index = taskList.indexOf(task);
+
+      if (index > -1) {
+        doneTask.push(task)
+        taskList.splice(index, 1);
+      }
+
+      renderTaskList();
+      renderDoneList();
+    });
+
+function renderDoneList() {
+  doneTaskElement.innerHTML = '';
+  doneTask.forEach(task => {
+    const doneLi = document.createElement('li')
+    doneLi.innerHTML = task 
+
+    doneTaskElement.appendChild(doneLi)
+
+    doneLi.style.textDecoration = 'line-through'
+
   })
+
+}
+
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    li.appendChild(doneButton);
+    doneTaskElement.appendChild(li);
+
+    taskListElement.appendChild(li);
+  });
 }
 
 function addTask() {
@@ -24,3 +75,9 @@ function addTask() {
   renderTaskList()
   inputTask.value = '';
 }
+
+inputTask.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addTask()
+  }
+})
