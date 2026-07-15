7b2a0ee7990966373e80f9f6a078b3d05a847a53
DanielGolonko <giziodyzio@gmail.com>
2026-07-15 19:26:29 +0200

    added done button, edit button, time and start doing priority and category of tasks

--- to-do-list.js
+++ to-do-list.js
@@ -1,8 +1,24 @@
 const inputTask = document.getElementById('inputTask');
 const taskListElement = document.getElementById('taskList');
 const doneTaskElement = document.getElementById('doneTask');
+const category = document.getElementById('category');
+const priority = document.getElementById('priority');
+
 
 let tasks = [];
+
+function filterTasks() {
+  const selectedCategory = category.value;
+  const selectedPriority = priority.value;
+
+  if (selectedCategory === 'all') {
+    renderTaskList();
+  } else if (selectedCategory === 'done') {
+    renderTaskList().style.display = 'none';
+    tasks = tasks.filter(task => task.done);
+    renderTaskList();
+  }
+}
 
 function saveTasks() {
   const stringifyTasks = JSON.stringify(tasks)
@@ -31,15 +47,34 @@ function renderTaskList() {
     const text = document.createElement('span');
     text.innerText = task.text;
 
+    const doneButton = document.createElement('button')
+    doneButton.innerText = 'Done'
+    doneButton.addEventListener('click', () => {
+      task.done = true;
+      saveTasks()
+      renderTaskList();
+    })
+
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Delete';
     deleteButton.addEventListener('click', () => {
       tasks = tasks.filter(t => t !== task);
       saveTasks()
       renderTaskList();
-
-
     })
+
+    const editButton = document.createElement('button')
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => {
+      const newText = prompt('Edit your task', task.text)
+      task.text = newText
+      saveTasks()
+      renderTaskList();
+    })
+
+    const date = document.createElement('span');
+    date.innerText = new Date().toLocaleString();
+
 
     if (task.done) {
       text.style.textDecoration = 'line-through';
@@ -51,15 +86,22 @@ function renderTaskList() {
       renderTaskList();
     });
 
+
+
     li.appendChild(checkbox);
     li.appendChild(text);
+    li.appendChild(doneButton);
     li.appendChild(deleteButton)
+    li.appendChild(editButton)
+    li.appendChild(date)
 
     if (task.done) {
       doneTaskElement.appendChild(li);
+      doneButton.style.display = 'none'
     } else {
       taskListElement.appendChild(li);
     }
+
 
 
   });
@@ -78,8 +120,6 @@ function addTask() {
   inputTask.value = '';
   saveTasks()
   renderTaskList();
-
-
 }
 
 inputTask.addEventListener('keydown', (event) => {
@@ -88,4 +128,6 @@ inputTask.addEventListener('keydown', (event) => {
 
   }
 });
+
+
 
