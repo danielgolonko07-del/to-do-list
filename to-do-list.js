e4a1de90602a77efc1a688077f8329f22c137a89
DanielGolonko <giziodyzio@gmail.com>
2026-07-17 21:32:01 +0200

    improve time functionality and add priority functionality

--- to-do-list.js
+++ to-do-list.js
@@ -1,24 +1,10 @@
 const inputTask = document.getElementById('inputTask');
 const taskListElement = document.getElementById('taskList');
 const doneTaskElement = document.getElementById('doneTask');
-const category = document.getElementById('category');
-const priority = document.getElementById('priority');
-
+let priorityRadios = document.getElementsByName('priority');
 
 let tasks = [];
 
-function filterTasks() {
-  const selectedCategory = category.value;
-  const selectedPriority = priority.value;
-
-  if (selectedCategory === 'all') {
-    renderTaskList();
-  } else if (selectedCategory === 'done') {
-    renderTaskList().style.display = 'none';
-    tasks = tasks.filter(task => task.done);
-    renderTaskList();
-  }
-}
 
 function saveTasks() {
   const stringifyTasks = JSON.stringify(tasks)
@@ -36,6 +22,15 @@ function renderTaskList() {
   taskListElement.innerHTML = '';
   doneTaskElement.innerHTML = '';
 
+  const priorityOrder = {
+    low: 1,
+    mid: 2,
+    high: 3
+  };
+
+  tasks.sort((a, b) => {
+    return priorityOrder[b.priority] - priorityOrder[a.priority];
+  });
   tasks.forEach(task => {
 
     const li = document.createElement('li');
@@ -51,6 +46,7 @@ function renderTaskList() {
     doneButton.innerText = 'Done'
     doneButton.addEventListener('click', () => {
       task.done = true;
+      task.date = completeDate
       saveTasks()
       renderTaskList();
     })
@@ -72,28 +68,47 @@ function renderTaskList() {
       renderTaskList();
     })
 
-    const date = document.createElement('span');
-    date.innerText = new Date().toLocaleString();
+    const showDate = document.createElement('span')
+    showDate.innerText = task.date
 
 
     if (task.done) {
       text.style.textDecoration = 'line-through';
     }
 
+    const completeDate = new Date().toLocaleString('pl-PL', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    })
+
     checkbox.addEventListener('change', () => {
       task.done = checkbox.checked;
+      task.date = completeDate
       saveTasks()
       renderTaskList();
     });
 
+    const priorityStars = document.createElement('span')
+
+    if (task.priority === 'low') {
+      priorityStars.innerHTML = '⭐'
+    } else if (task.priority === 'mid') {
+      priorityStars.innerHTML = '⭐⭐'
+    } else {
+      priorityStars.innerHTML = '⭐⭐⭐'
+    }
 
 
     li.appendChild(checkbox);
     li.appendChild(text);
+    li.appendChild(priorityStars);
     li.appendChild(doneButton);
     li.appendChild(deleteButton)
     li.appendChild(editButton)
-    li.appendChild(date)
+    li.appendChild(showDate)
 
     if (task.done) {
       doneTaskElement.appendChild(li);
@@ -110,11 +125,28 @@ function renderTaskList() {
 function addTask() {
   const task = inputTask.value.trim();
 
+  if (priorityRadios[0].checked) {
+    priority = 'low'
+  } else if (priorityRadios[1].checked) {
+    priority = 'mid'
+  } else if (priorityRadios[2].checked) {
+    priority = 'high'
+  }
+
+
   if (task === '') return;
 
   tasks.push({
     text: task,
-    done: false
+    done: false,
+    date: new Date().toLocaleString('pl-PL', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    }),
+    priority: priority
   });
 
   inputTask.value = '';
@@ -128,6 +160,10 @@ inputTask.addEventListener('keydown', (event) => {
 
   }
 });
+
+function filterTasks() {
+
+}
 
 
 
